627ffc5f1604ac2211509c08a0d33decba0be4c7
Light <prakashnarasanagi22@gmail.com>
2019-06-27 23:16:12 +0530

    stage 2 complete

--- ksp/pages/urls.py
+++ ksp/pages/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('',views.base,name='base'),
     path('base/',views.base,name='base'),
     path('upload',views.criminal_view,name='upload'),
-    path('success',views.success, name = 'success')
-    
-]
+    path('success',views.success, name = 'success'),
+    path('result',views.result,name = 'result')
 
+]
--- ksp/pages/views.py
+++ ksp/pages/views.py
@@ -1,50 +1,87 @@
-from django.shortcuts import render,redirect
-from django.http import HttpResponseRedirect,HttpResponse
+from django.shortcuts import render, redirect, render_to_response
+from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import path, include
 from .forms import *
 from pages.criminal_script.CriminalRecognition import *
 import face_recognition
+import time
+from django.template import loader, Context
+
 
 def base(request):
-   form = CriminalForm()
-   return render(request, 'index.html', {'form': form})
+    form = CriminalForm()
+    return render(request, 'index.html', {'form': form})
 
 
-def criminal_view(request): 
-  
-    if request.method == 'POST': 
-        form = CriminalForm(request.POST, request.FILES) 
-  
+def import_iter(img):
+    np_im = face_recognition.load_image_file(img)
+    criminal_instance = CriminalRecognition()
+
+    t = loader.get_template('result.html')
+    c = Context()
+    yield t.render()
+
+    yield "<div class='container'><br><h1>Matched Images</h1><br><div style='width:1100px;display:flex; flex-wrap: wrap;'>"
+    if criminal_instance.validator(np_im):
+        print("check iter")
+        import os
+        filenames = (os.listdir("static/images/Test Images/"))
+        for i in filenames:
+            #print("static/images/Test Images/"+str(i))
+            img2 = face_recognition.load_image_file(
+                "static/images/Test Images/"+str(i))
+            # print(img2)
+            if criminal_instance.face_compare(np_im, img2):
+                print("Matched image:  ", i)
+                yield "<div class='img-uploader' style='height:300px; padding:20px;overflow-y:hidden'><img style='width:300px;float:left;' src='static/images/Test Images/"+i+"' alt="+i+" /></div>"
+        yield "</div></div>"
+    else:
+        return HttpResponse('no face detected')
+
+
+def criminal_view(request):
+
+    if request.method == 'POST':
+        form = CriminalForm(request.POST, request.FILES)
+
         if form.is_valid():
-            img=(request.FILES.get('criminal_Img'))
-            
+            img = (request.FILES.get('criminal_Img'))
+            return HttpResponse(import_iter(img))
             #from PIL import Image
             #import numpy
             #im = Image.open(img)
             #np_im = numpy.array(im)
-            #print(np_im)
-            np_im=face_recognition.load_image_file(img)
-            #print(np_im)
-            criminal_instance = CriminalRecognition()
-            if criminal_instance.validator(np_im):
-                print("in")
-                import os
-                filenames=(os.listdir("Test Images/"))
-                for i in filenames:
-                    print("Test Images/"+str(i))
-                    img2=face_recognition.load_image_file("Test Images/"+str(i))
-                    #print(img2)
-                    if criminal_instance.face_compare(np_im,img2):
-                        print(i)
+            # print(np_im)
+            # np_im=face_recognition.load_image_file(img)
+            # print(np_im)
+            #criminal_instance = CriminalRecognition()
+            # if criminal_instance.validator(np_im):
+            #    print("in")
+            #    import os
+            #    filenames=(os.listdir("static/images/Test Images/"))
+            #    for i in filenames:
+            #        print("static/images/Test Images/"+str(i))
+            #        img2=face_recognition.load_image_file("static/images/Test Images/"+str(i))
+            #        #print(img2)
+            #        if criminal_instance.face_compare(np_im,img2):
+            #            print(i)
 
-            else:
-                return HttpResponse('no face detected')
+    else:
+        form = CriminalForm()
+    return render(request, 'upload.html', {'form': form})
 
-            return redirect('success') 
-    else: 
-        form = CriminalForm() 
-    return render(request, 'upload.html', {'form' : form}) 
-  
-  
-def success(request): 
-    return HttpResponse('successfuly uploaded') 
+
+def success(request):
+    return HttpResponse('successfuly uploaded')
+
+# def gen():
+#    frame = [1,2,3]
+#    for i in frame:
+#        yield { 'value' : i}
+
+
+def result(request):
+    if request.method == "POST":
+        return HttpResponse(import_iter())
+    else:
+        return render(request, 'result.html', {'value': 10})
